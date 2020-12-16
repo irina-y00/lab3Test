0fe2845399685ee67850899c59d58022d28e3e3a
Irina <yarygina.irina00@gmail.com>
2020-12-16 11:34:39 +0300

    добавлена функция вставки и вспомогательные для нее функции

--- RBTree.py
+++ RBTree.py
@@ -11,3 +11,93 @@ class RBTree:
     def __init__(self):
         self.nil = RBTreeNode(-1)
         self.root = self.nil
+
+
+class Solution:
+    def LeftRotate(self, T, x):
+        y = x.right
+        x.right = y.left
+        if y.left != T.nil:
+            y.left.parent = x
+        y.parent = x.parent
+        if x.parent == T.nil:
+            T.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    def RightRotate(self, T, x):
+        y = x.left
+        x.left = y.right
+        if y.right != T.nil:
+            y.right.parent = x
+        y.parent = x.parent
+        if x.parent == T.nil:
+            T.root = y
+        elif x == x.parent.right:
+            x.parent.right = y
+        else:
+            x.parent.left = y
+        y.right = x
+        x.parent = y
+
+    def RBInsertFixup(self, T, z):
+        while z.parent.color == 'red':
+            if z.parent == z.parent.parent.left:
+                y = z.parent.parent.right
+                if y.color == 'red':
+                    z.parent.color = 'black'
+                    y.color = 'black'
+                    z.parent.parent.color = 'red'
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.right:
+                        z = z.parent
+                        self.LeftRotate(T, z)
+                    z.parent.color = 'black'
+                    z.parent.parent.color = 'red'
+                    self.RightRotate(T, z.parent.parent)
+            else:
+                y = z.parent.parent.left
+                if y.color == 'red':
+                    z.parent.color = 'black'
+                    y.color = 'black'
+                    z.parent.parent.color = 'red'
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.left:
+                        z = z.parent
+                        self.RightRotate(T, z)
+                    z.parent.color = 'black'
+                    z.parent.parent.color = 'red'
+                    self.LeftRotate(T, z.parent.parent)
+        T.root.color = 'black'
+
+    def RBInsert(self, T, z):
+        z.left = T.nil
+        z.right = T.nil
+        z.parent = T.nil
+
+        y = T.nil
+        x = T.root
+        while x != T.nil:
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+        z.parent = y
+        if y == T.nil:
+            T.root = z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+        z.left = T.nil
+        z.right = T.nil
+        z.color = 'red'
+        self.RBInsertFixup(T, z)
+
